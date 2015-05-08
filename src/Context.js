5dfa69549055e4ea707cdaeb6d95ae4789c7ddfd
Stewart MacKenzie-Leigh <stewart@stugo.co.uk>
2015-05-08 12:41:56 +0100

    Add param support

--- src/Context.js
+++ src/Context.js
@@ -1,44 +1,44 @@
 
 var methods = [
   'checkout', 'connect', 'copy', 'delete', 'get', 'head', 'lock', 'merge', 'mkactivity', 'mkcol', 'move',
-  'm-search', 'notify', 'options', 'patch', 'post', 'propfind', 'proppatch', 'purge', 'put', 'report',
+  'm-search', 'notify', 'options', 'param', 'patch', 'post', 'propfind', 'proppatch', 'purge', 'put', 'report',
   'search', 'subscribe', 'trace', 'unlock', 'unsubscribe'
 ];
 
 export default class Context {
-  
+
   constructor(router, context) {
     this._router = router;
     this._context = context;
-    
+
     // install the HTTP methods
     for (let i in methods) {
       var method = methods[i];
       this[method] = this.request.bind(this, method);
     }
   }
-  
-  
+
+
   request(method, route, ...args) {
     let context = this._context;
-    
+
     args = args.map((fn) => function (request, response, next) {
-      let result = fn.call(context, request, response, next);
-      
+      let result = fn.apply(context, arguments);
+
       if (typeof result !== 'undefined' && result !== null && typeof result.then === 'function') {
         result.then(void 0, next);
       }
     });
-    
+
     // register the route with express
     args.unshift(route);
     this._router[method].apply(this._router, args);
-    
+
     // allow chaining
     return this;
   }
-  
-  
+
+
   getRouter() {
     return this._router;
   }
--- test/test.js
+++ test/test.js
@@ -2,20 +2,20 @@
 import {expect} from 'chai';
 import supertest from 'supertest-as-promised';
 import express from 'express';
-import Router, {Context} from '../src/router';
+import Router, {Context} from '../src/Router';
 
 
 describe('Router', function () {
   var wrappedRouter;
   var app;
-  
+
   beforeEach(function () {
     let router = express.Router();
     wrappedRouter = new Router(router);
     app = express();
     app.use(router);
   });
-  
+
   it('should call with correct context', async function () {
     wrappedRouter
       .context({name: 'context'})
@@ -24,14 +24,14 @@ describe('Router', function () {
         expect(this.name).to.equal('context');
         response.send(value.toString());
       });
-    
+
     let response = await supertest(app)
       .get('/test')
       .expect(200);
-    
+
     expect(response.text).to.equal('5');
   });
-  
+
   it('should throw errors', async function () {
     wrappedRouter
       .get('/test', async function (request, response) {
@@ -39,16 +39,16 @@ describe('Router', function () {
         throw new Error();
         response.send();
       });
-    
+
     await supertest(app)
       .get('/test')
       .expect(500);
   });
-  
+
   it('should export context', function () {
     expect(Context).to.exist;
   });
-  
+
   it('should allow chaining', function () {
     expect((new Router(express.Router())).context({}).get('',function () {}).get).to.exist;
   });
